739b0d80cd19f5b866d63329856f5b13b628d0b4
Niels Berglund <niels.it.berglund@gmail.com>
2022-12-04 09:58:24 +0200

    Added scratchfile directory and file.

---
+++ scratchpad/brew-install.sh
@@ -0,0 +1,49 @@
+brew tap
+brew tap azure/functions
+brew tap homebrew/cask
+brew tap homebrew/core
+brew tap romkatv/powerlevel10k
+brew tap isen-ng/dotnet-sdk-versions
+
+brew update
+
+brew install mas
+
+brew install --cask firefox
+brew install --cask sublime-text
+brew install --cask vlc
+brew install --cask snagit
+brew install pandoc
+brew install openjdk
+brew install maven
+brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask postman
+brew install --cask iterm2
+brew install python@3.9
+brew install azure-cli
+brew install --cask grammarly
+brew install --cask alfred
+brew install --cask azure-data-studio
+brew install --cask cleanmymac
+
+
+brew install --cask confluent-cli
+
+brew install powerlevel10k
+
+brew tap isen-ng/dotnet-sdk-versions
+
+brew install --cask dotnet-sdk6-0-400
+
+
+brew install --cask microsoft-auto-update
+brew install --cask microsoft-excel
+brew install --cask microsoft-word
+brew install --cask microsoft-powerpoint
+
+
+
+
+
+
