091b7f5b49ccd72fadcf7693a31bb729809f7940
Niels Berglund <niels.it.berglund@gmail.com>
2023-09-21 04:23:33 +0200

    edited some dotfiles.

---
+++ scripts/brew-standalone2.sh
@@ -0,0 +1,9 @@
+info "Prompting for sudo pwd"
+if sudo -v; then
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  success "Sudo credentials updated."
+else
+  error "Could not get sudo credentials."
+fi
+
+brew bundle --file=~/repos/dotfiles/Brewfile
--- scripts/brew.sh
+++
@@ -1,5 +0,0 @@
-install_brew_packages() {
-
-  brew bundle --file=~/repos/dotfiles/Brewfile
-  brew install --cask alt-tab
-}
